804a9b68559d44d3894ebd83d0040d2f689db026
Artemie Amari <yaroamari@gmail.com>
2020-08-23 17:47:17 -0400

    camper

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react'
 export default class Contact extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <div class='login-container'>
+        <div class='contact1'>
+        <h2>Contact</h2>
+          <h1><a class='phone' href='tel:3474006623'>347-400-6623</a></h1>
+            <h3><a class='phone' href="mailto:convertedcamper@gmail.com">ConvertedCamper@gmail.com</a></h3>
+        </div>
+        </div>
     )
   }
 }
