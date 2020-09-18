353a4802a945421e0317b0bad1cec36cb92ff865
Artemie Amari <yaroamari@gmail.com>
2020-09-18 12:08:32 -0400

    commit

--- src/components/Team.js
+++ src/components/Team.js
@@ -15,7 +15,7 @@ export default class Team extends Component {
           <div class='bio-text'>
           <h3>Marissa Cole</h3>
         <h4>Head of Design</h4>
-          <p class='blurb'>Marissa has an eye for making environemnts as simplistic and functional as possible. She is a graduate from Maryland University where she graduated with a B.S. in Marketing.  </p>
+          <p class='blurb'>Marissa has an eye for making environemnts as simplistic and functional as possible. She is a graduate from Maryland University where she graduated with a B.S. in Marketing. She worked as a member of the PJT Marketing team. </p>
           </div>
         </div>
         <h1 class='page-title'>Web Development</h1>
@@ -25,7 +25,7 @@ export default class Team extends Component {
             <h3>Artemie Amari</h3>
             <a  class='portfolio-link' target="_blank" rel="noopener noreferrer" href='https://www.artemieamari.com/#portfolio'>Portfolio</a>
         <h4>Full Stack Web Developer</h4>
-          <p class='blurb'>Artemie has been pursuing web development since highschool when he took his first program course. He then graduated from Georgia Southern with a degree in Finance and thereafter attended General Assembly's web development bootcamp. He has developed countless websites since then.  </p>
+          <p class='blurb'>Artemie has been pursuing web development since highschool when he first started programming. He has been developing ever since.  </p>
           </div>
         </div>
         </div>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,33 +1,40 @@
 import React, { Component } from 'react'
 import Nav from './Testnav'
-import Ball from '../images/Tennis-Ball-Download-Free-PNG.png'
-import Mind from '../images/mind.svg'
-import Camper from '../images/Floral-Volkswagen-Camper.svg'
+import Ball from '../images/Tennis Coaches Of NYC.png'
+import Mind from '../images/Mind Guided Meditation.png'
+import Camper from '../images/ConvertedCamper.io.png'
+import Dog from '../images/Dog Sitting NYC.png'
 
 export default class Work extends Component {
   render() {
     return (
       <>
         <Nav/>
-      <div class='page1'>
+        <div class='page1'>
           <div class='portfolio-container'>
-            <h3>Our Work</h3>
-            <h2>Some highlights of our favorite projects we've done for forward thinking clients.</h2>
-        <div class='work-piece'>
-        <h3>Tennis Coaches of NYC</h3>
-              <a href='http://tenniscoachesofnyc.com/'><img src={Ball}/></a>
-          </div>
-          <div class='work-piece'>
-        <h3 >Mind Guided Meditation</h3>
-              <a href='https://www.mindguidedmeditation.com/'><img src={Mind}/></a>
-          </div>
-          <div class='work-piece'>
-        <h3 >Converted Camper</h3>
-              <a href='https://www.convertedcamper.io/'><img src={Camper}/></a>
-          </div>
+            <div class='portfolio-intro'>
+              <h3>Our Work</h3>
+              <h2 class='intro'>Some highlights of our favorite projects we've done for forward thinking clients.</h2>
+            </div>
+            <div class='work-piece-tennis'>
+              <h3 class='title-tennis'>Tennis Coaches of NYC</h3>
+              <a href='http://tenniscoachesofnyc.com/'><img class='portfolio-img' src={Ball}/></a>
+            </div>
+            <div class='work-piece-mind'>
+              <h3 class='title-mind'>Mind Guided Meditation</h3>
+              <a href='https://www.mindguidedmeditation.com/'><img class='portfolio-img' src={Mind}/></a>
+            </div>
+            <div class='work-piece-camper'>
+              <h3 class='title-camper'>Converted Camper</h3>
+              <a href='https://www.convertedcamper.io/'><img class='portfolio-img' src={Camper}/></a>
+            </div>
+            <div class='work-piece-dog'>
+              <h3 class='title-dog'>Dog Sitting NYC</h3>
+              <a href='https://www.dogsittingnyc.com/'><img class='portfolio-img' src={Dog}/></a>
+            </div>
           </div>
         </div>
-        </>
+      </>
     )
   }
 }
